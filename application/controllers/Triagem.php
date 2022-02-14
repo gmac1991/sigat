5f43030b2105c1eebaf58b510e2e716bcbcc956c
Your Name <you@example.com>
2022-02-14 20:19:04 -0300

    triagem grid fix

--- application/controllers/Triagem.php
+++ application/controllers/Triagem.php
@@ -14,12 +14,12 @@ class Triagem extends CI_Controller {
     
   }
 
-  public function index($id_chamado = NULL) { //controle dos chamados
+  public function index($id_triagem = NULL) { //controle dos chamados
     if (isset($_SESSION['id_usuario'])) {
     
 		$dados = NULL;
 		
-		$dados['triagem'] = $this->consultas_model->buscaTriagem($id_chamado); //traz chamado migrado
+		$dados['triagem'] = $this->consultas_model->buscaTriagem($id_triagem); //traz chamado migrado
 		
 		if (isset($dados['triagem'])) { // se o chamado existir
 			
@@ -68,13 +68,13 @@ public function listar_triagem() {
 		
 	
 
-		$lista_painel['data'][] = array(0 => $linha->id_chamado,
-                              1 => $linha->nome_solicitante_chamado,
-							  2 => $linha->email_chamado,
-                              3 => $linha->data_chamado,
-                              4 => $linha->ticket_chamado,
-                              5 => $linha->status_chamado,
-                              6 => "<a href=\"" . base_url('triagem/' . $linha->id_chamado) . 
+		$lista_painel['data'][] = array(
+                              0 => $linha->id_triagem,
+                              1 => $linha->ticket_triagem,
+                              2 => $linha->data_chamado,
+                              3 => $linha->nome_solicitante_chamado,
+							                4 => $linha->email_chamado,
+                              5 => "<a href=\"" . base_url('triagem/' . $linha->id_chamado) . 
                               "\" rel=\"noopener\" role=\"button\"" .
                               " class=\"d-block btn btn-sm btn-info\"><i class=\"fas fa-search\"></i></a> "); //detalhes
 
