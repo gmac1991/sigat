58b2e9d72c29ca22f83ffe301c22dc542c8e5004
GXBM <gmacedo@sorocaba.sp.gov.br>
2022-04-19 13:59:01 -0300

    js minify

--- application/views/templates/rodape.php
+++ application/views/templates/rodape.php
@@ -53,7 +53,7 @@ if (isset($triagem)) {
   <script src="<?= base_url("js/jsgrid.min.js"); ?>"></script>
   <script src="<?= base_url("fa/js/all.min.js")?>"></script>
   <script src="<?= base_url("js/moment.min.js")?>"></script>
-  <script src="<?= base_url("js/principal.js")?>"></script>
+  <script src="<?= base_url("js/principal.min.js")?>"></script>
 
 <?php
 
