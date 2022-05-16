282337cc0bec099b4492d13a0f908e7b1b03d2e8
GXBM <gmacedo@sorocaba.sp.gov.br>
2022-05-16 13:36:05 -0300

    deploy fix

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session','phpmailer/PHPMailer','phpmailer/smtp','phpmailer/exception');
+$autoload['libraries'] = array('database','session','phpmailer/PhpMailer','phpmailer/Smtp','phpmailer/Exception');
 
 /*
 | -------------------------------------------------------------------
