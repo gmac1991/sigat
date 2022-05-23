4919e7819bc4ae10eb23a9828e872fb168fe3e50
GXBM <gmacedo@sorocaba.sp.gov.br>
2022-05-23 11:01:04 -0300

    v1.0.03

--- index.php
+++ index.php
@@ -177,7 +177,7 @@ switch (ENVIRONMENT)
  * Un-comment the $assign_to_config array below to use this feature
  */
 	$assign_to_config['versao'] = '1.0.0.3';
-	$assign_to_config['caminho_termos'] = '../../termos/';
+	$assign_to_config['caminho_termos'] = 'termos/';
 	$assign_to_config['api_sim'] = "https://sistemas.sorocaba.sp.gov.br/acesso_patrimonio/api/patrimonio/";
 
 
