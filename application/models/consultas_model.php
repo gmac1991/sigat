be309121e725b0482e92d5efda72c533f0895186
GXBM <gmacedo@sorocaba.sp.gov.br>
2022-05-24 18:50:17 -0300

    v1.0.0.5

--- application/models/consultas_model.php
+++ application/models/consultas_model.php
@@ -43,7 +43,7 @@ class Consultas_model extends CI_Model {
             $q .= " id_fila_chamado > 0 ";
         }
 
-        $q .= ' order by nome_responsavel is not NULL, status_chamado <> \'ABERTO\',status_chamado <> \'FECHADO\', data_chamado';
+        $q .= ' order by data_chamado, nome_responsavel is not NULL, status_chamado <> \'ABERTO\',status_chamado <> \'FECHADO\'';
 
         return $this->db->query($q)->result();
     }
